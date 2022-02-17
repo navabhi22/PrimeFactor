e92db4dd7921fff7f918a3df7778ce2c1e762423
navabhi22 <navabhi22@gmail.com>
2022-02-17 18:14:29 +0530

    add program.cs to get prime factor of an integer

---
+++ Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace PrimeFactors
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter the value of N ");
+            int N = Convert.ToInt32(Console.ReadLine());
+
+            //PrimeNumber.PrimeNum(N);
+
+            PrimeFactors.PrimeFactor(N);
+        }
+    }
+}
