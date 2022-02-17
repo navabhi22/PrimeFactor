5785e2dd4bc54a56f230e719815c48e31bd82bce
navabhi22 <navabhi22@gmail.com>
2022-02-17 18:20:07 +0530

    adding class prime number to check for prime factors

---
+++ PrimeNumber.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PrimeFactors
+{
+    public class PrimeNumber
+    {
+        public static bool PrimeNum(int N)
+        {
+            int numOfFact = 0;
+            for (int i = 1; i <= N; i++)
+            {
+                int rimainder = N % i;
+                
+                if (rimainder == 0)
+                {
+                    numOfFact++;
+                }
+               
+            }
+            
+            if (numOfFact == 2)
+            { 
+                return(true);
+                //Console.WriteLine("prime");
+            }
+            else
+            {
+                return (false);
+                //Console.WriteLine("not prime");
+            }
+        }
+    }
+}
